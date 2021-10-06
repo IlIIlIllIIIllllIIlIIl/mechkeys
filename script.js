ba2abdccd35ecca519b3727bcd66161f6b342cac
fucketf <91749950+fucketf@users.noreply.github.com>
2021-10-06 08:27:58 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function getURL(){
+  var website = document.getElementById("website").value;
+  getHttp =   website.substring(0, 8);
+  if ( getHttp != "https://"){
+    website = 'https://' + website;
+  }
+  const request = new Request(website);
+  const url = request.url;
+  console.log(url);
+  window.open("https://6e84d2e0-65a0-4808-9874-5f9e48bf14a9.id.repl.co/?URL="+url);
+  //const sourceSite = 'view-source:'+url;
+  
+}
