1331b41f3db619078aa07320266e5a8140891794
fetf <91749950+fetf@users.noreply.github.com>
2021-10-07 13:02:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ function getURL(){
   var website = document.getElementById("website").value;
   getHttp =   website.substring(0, 8);
   getWWW = website.indexOf('www');
-  if (getWWW == -1){
-    website= 'www.'+website;
-  }
   if ( getHttp != "https://"){
-    website = 'https://' + website;
+    if (getWWW ==-1){
+      website = 'https://www.' + website;
+    }
+    else{
+      website = 'https://' + website;
+    }
   }
-  
   const request = new Request(website);
   const url = request.url;
   console.log(url);
